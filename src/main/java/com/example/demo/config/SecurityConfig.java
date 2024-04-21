a5d25ee22a34ed0c3c2e4d0391772ca1d94fef96
Harley Diniz <fhdfm85@gmail.com>
2024-04-21 17:44:32 -0300

    ajuste no mapeamento de rotas abertas e seguras

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -36,9 +36,8 @@ public class SecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/admin/*").authenticated())
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/aluno/*").authenticated())
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .authorizeHttpRequests(auth -> auth.requestMatchers(
+                    "/api/admin/*", "/api/aluno/*").authenticated().anyRequest().permitAll())
                 .httpBasic(Customizer.withDefaults())
                 .oauth2ResourceServer(conf -> conf.jwt(Customizer.withDefaults()));
         return http.build();
