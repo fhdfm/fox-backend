4b91094fd33c049bf17cea9a8cab927c5a476964
Harley Diniz <fhdfm85@gmail.com>
2024-05-31 14:24:45 -0300

    Exception Handler para tratar erros de banco de dados.

--- src/main/java/br/com/foxconcursos/config/FoxExceptionHandler.java
+++ src/main/java/br/com/foxconcursos/config/FoxExceptionHandler.java
@@ -1,5 +1,9 @@
 package br.com.foxconcursos.config;
 
+import java.sql.SQLException;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,6 +14,33 @@ public class FoxExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<Object> handle(IllegalArgumentException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolationException(
+        DataIntegrityViolationException e) {
+
+        SQLException sqlException = (SQLException) e.getCause();
+
+        String mensagem = "Erro de integridade de dados";
+
+        if (sqlException != null) {
+            String sqlState = sqlException.getSQLState();
+            switch (sqlState) {
+                case "23503": // foreign key violation
+                    mensagem = "Erro: O registro não pode ser excluído pois "
+                        + "está sendo referenciado por outros registros";
+                    break;
+                case "23505": // unique constraint violation
+                    mensagem = "Erro: Já existe um registro com os dados informados";
+                    break;
+                default:
+                    mensagem = "Erro: " + sqlException.getMessage();
+                    break;
+            }
+        }
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(mensagem);
     }
 
 }
