45fe1088eea763b6247346ec83f0cf5a30e0f0f9
Harley Diniz <fhdfm85@gmail.com>
2024-12-19 07:57:42 -0300

    .

--- src/main/java/br/com/foxconcursos/controllers/AulaController.java
+++ src/main/java/br/com/foxconcursos/controllers/AulaController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +22,7 @@ import br.com.foxconcursos.dto.AulaConteudoRequest;
 import br.com.foxconcursos.dto.AulaRequest;
 import br.com.foxconcursos.dto.AulaResponse;
 import br.com.foxconcursos.services.AulaService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 
 @RestController
 public class AulaController {
