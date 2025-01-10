89796433ed744379eade9a056e745f3f9d62138b
Paulo Mesquita <paulomesquita0@gmail.com>
2025-01-10 14:00:54 -0300

    PathVariable

--- src/main/java/br/com/foxconcursos/controllers/CursoAulaAlunoController.java
+++ src/main/java/br/com/foxconcursos/controllers/CursoAulaAlunoController.java
@@ -8,12 +8,12 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.foxconcursos.dto.CursoAlunoResponse;
 import br.com.foxconcursos.services.CursoAlunoService;
-import jakarta.websocket.server.PathParam;
 
 @RestController
 @RequestMapping(value = "/api/alunos/curso", 
@@ -29,21 +29,21 @@ public class CursoAulaAlunoController {
 
     @PreAuthorize("hasAuthority('SCOPE_ROLE_ALUNO')")
     @GetMapping("/{cursoId}/aula/{aulaId}/complete")
-    public ResponseEntity<String> complete(@PathParam("cursoId") UUID cursoId, @PathParam("aulaId") UUID aulaId) {
+    public ResponseEntity<String> complete(@PathVariable("cursoId") UUID cursoId, @PathVariable("aulaId") UUID aulaId) {
         this.service.salvarProgresso(cursoId, aulaId);
         return ResponseEntity.status(HttpStatus.OK).body("Progresso salvo com sucesso.");
     }
 
     @PreAuthorize("hasAuthority('SCOPE_ROLE_ALUNO')")
     @GetMapping("/{cursoId}/start")
-    public ResponseEntity<CursoAlunoResponse> iniciar(@PathParam("cursoId") UUID cursoId) throws IOException {
+    public ResponseEntity<CursoAlunoResponse> iniciar(@PathVariable("cursoId") UUID cursoId) throws IOException {
         CursoAlunoResponse response = this.service.obterCurso(cursoId);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @PreAuthorize("hasAuthority('SCOPE_ROLE_ALUNO')")
-    @GetMapping("/{cursoId}/aula/{aulaId}/")
-    public ResponseEntity<CursoAlunoResponse> mudarAula(@PathParam("cursoId") UUID cursoId, @PathParam("aulaId") UUID aulaId) throws IOException {
+    @GetMapping("/{cursoId}/aula/{aulaId}")
+    public ResponseEntity<CursoAlunoResponse> mudarAula(@PathVariable("cursoId") UUID cursoId, @PathVariable("aulaId") UUID aulaId) throws IOException {
         CursoAlunoResponse response = this.service.obterCurso(cursoId, aulaId);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
