92d916ddb7ce1c1c3d59999c7ecca8bec696c509
Harley Diniz <fhdfm85@gmail.com>
2025-04-13 14:33:16 -0300

    ,

--- src/main/java/br/com/foxconcursos/repositories/AlternativaRepository.java
+++ src/main/java/br/com/foxconcursos/repositories/AlternativaRepository.java
@@ -1,10 +1,13 @@
 package br.com.foxconcursos.repositories;
 
+import java.util.List;
 import java.util.UUID;
 
 import br.com.foxconcursos.domain.Alternativa;
 import br.com.foxconcursos.repositories.custom.CustomCrudRepository;
 
 public interface AlternativaRepository extends CustomCrudRepository<Alternativa, UUID> {
+
+    List<Alternativa> findByQuestaoId(UUID questaoId);
     
 }
